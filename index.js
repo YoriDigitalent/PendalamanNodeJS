3f7a131b19875ba3a0055241536191248b15f2a2
YoriDigitalent <yori.digitalent@gmail.com>
2020-10-12 09:57:15 +0700

    initial commit dan tambah layout

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import hbs from 'hbs'
+import path from 'path'
+import morgan from 'morgan'
+
+const __dirname = path.resolve()
+
+const app = express()
+
+app.set('views', __dirname + '/layouts')
+app.set('view engine', 'html')
+app.engine('html', hbs.__express)
+
+// log incoming reques
+app.use(morgan('combined'))
+
+//serve static file
+app.use('/assets', express.static(__dirname + '/assets'))
+
+app.get('/', (req, res, next) => {
+    res.send({success: true})
+})
+
+app.get('/product', (req, res, next) => {
+    res.render('product')
+})
+
+app.use((err, req, res, next) => {
+    res.send(err, message)
+})
+
+app.listen(8010, () => {
+    console.log('App listen on port 8010')
+})
