a70305c9b8c0122a2e959bcce65757611f28fa34
YoriDigitalent <yori.digitalent@gmail.com>
2020-10-12 12:10:55 +0700

    tambah file photo

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import path from 'path'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import {getProduct, initDatabase, initTable, insertProduct} from './database.js'
-import { get } from 'http'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 const __dirname = path.resolve()
 
@@ -16,6 +17,10 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+//use file upload
+app.use(fileUpload())
+
+
 // log incoming reques
 app.use(morgan('combined'))
 
@@ -24,6 +29,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 //serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
     res.send({success: true})
@@ -59,11 +65,23 @@ app.get('/add-product', (req, res, next) => {
 //method post
 app.post('/add-product', (req, res, next) => {
     console.log('Request', req.body)
+    console.log('File', req.files)
+
+    //get fileName
+    const fileName = req.files.photo.name
+
+    fs.writeFile(path.join(__dirname, '/files', fileName), req.files.photo.data, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+
     //insert product
-    insertProduct(db, req.body.name, parseInt(req.body.price), '-')
+    insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
 
     //redirect
     res.redirect('/product')
+    })
 })
 
 app.use((err, req, res, next) => {
