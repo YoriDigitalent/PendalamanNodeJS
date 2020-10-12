28c3a387665c95215af20bbec52453a4f10cc824
YoriDigitalent <yori.digitalent@gmail.com>
2020-10-12 10:42:16 +0700

    Tambah body parser di index.js dan tambah insert product di database.js

--- database.js
+++ database.js
@@ -16,15 +16,47 @@ export function initDatabase() {
  *  
  * @param {sqlite.Database} db 
  */
-
- export function initTable(db) {
-     db.serialize(() => {
-         db.run (`CREATE TABLE IF NOT EXISTS product (
-             id INTEGER PRIMARY KEY AUTOINCREMENT,
-             photo TEXT NOT NULL,
-             name VARCHAR(56) NOT NULL,
-             price INTEGER NOT NULL
-         );`)
-     })
-
+export function initTable(db) {
+    db.serialize(() => {
+        db.run (`CREATE TABLE IF NOT EXISTS product (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            photo TEXT NOT NULL,
+            name VARCHAR(56) NOT NULL,
+            price INTEGER NOT NULL
+        );`)
+    })
 }
+
+/** 
+ *  
+ * @param {sqlite.Database} db
+ * @param {string} name
+ * @param {number} price
+ * @param {string} photo
+ */
+export function insertProduct(db, name, price, photo) {
+    db.run('INSERT INTO product (photo,name,price)', { name, price, photo }, (err) => {
+      if(err) {
+        console.log(err)
+        throw err
+      }
+
+      console.log('product saved')
+    })
+  }
+
+
+/**
+ * 
+ * @param {mysql.Connection} dbConnection 
+ */
+export function getProduct(dbConnection) {
+    dbConnection.query('SELECT * FROM product', (err, result) => {
+      if(err) {
+        console.log(err)
+        throw err
+      }
+      console.log(result)
+      return result
+    })
+  }
